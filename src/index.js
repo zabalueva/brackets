3e38711ff3286ba6f2bbbf81978a23fdeb8ed260
zabalueva <age_eva@mail.ru>
2021-02-25 14:13:47 +0300

    feat: passing 18/20

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-
 	let bracketsConfigAll = [];
-	let bracketsConfigAllList = [];
-	let strArr = Array.from(str);
+    let bracketsConfigAllList = [];
+    let stack = [];
 
-	if (str.length % 2 == 0) {
-		for (let i = 0; i < bracketsConfig.length; i++) {
-			bracketsConfigAll[i] = bracketsConfig[i];
-			bracketsConfigAllList = bracketsConfigAll.toString().split(",");
-		}
-		/* console.log(strArr);
-		console.log(bracketsConfigAllList); */
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		bracketsConfigAll[i] = bracketsConfig[i];
+		bracketsConfigAllList = bracketsConfigAll.toString().split(",");
+	}
 
-		for (let j = 0; j < strArr.length; j++) {
+	/* console.log(str);
+	console.log(brackets); */
 
-			for (let i = 0; i < strArr.length - 1; i++) {
-				let index = bracketsConfigAllList.indexOf(strArr[i]);
+	for (let item of str) {
+		let bracketsIndex = bracketsConfigAllList.indexOf(item);
 
-				if (strArr[i + 1] == bracketsConfigAllList[index + 1] ) {
-					strArr.splice(i, 2);
-				}
+		if (bracketsIndex % 2 == 0) {
+            if (bracketsConfigAllList[bracketsConfigAllList.indexOf(item)]
+                == bracketsConfigAllList[bracketsConfigAllList.indexOf(item) + 1]) {
 
-				if (strArr.length !== 0) {
-					for (let i = 0; i < strArr.length - 1; i++) {
-						let index = bracketsConfigAllList.indexOf(strArr[i]);
 
-						if (strArr[i + 1] == bracketsConfigAllList[index + 1]) {
-							strArr.splice(i, 2);
-						}
-					}
+				if ((str.indexOf(item) - str.lastIndexOf(item)) % 2 == 0) {
+					return false;
+                }
+                if (str[str.indexOf(item)] == str[str.indexOf(item) + 1]) {
+                    Array.from(str).splice(str.indexOf(item), 2);
+                }
+
+				stack.push(bracketsIndex + 1);
+				stack.pop();
+			} else {
+				stack.push(bracketsIndex + 1);
+			}
+				/* console.log(stack); */
+			} else {
+				if (stack.pop() !== bracketsIndex) {
+					return false;
 				}
 			}
 		}
-		}
-	return strArr.length == 0;
+		return stack.length === 0;
 }
